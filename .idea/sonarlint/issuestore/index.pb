f4cd79db47f062d5e99eda08032643df3f7291c8
nadirjin <gin.nader94@gmail.com>
2017-02-07 15:10:53 -0500

    Fixed some errors that SonarQube found and completed Lab 2.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,11 +3,11 @@
 	file.json,a\4\a4a181d6be052d81e45861d459bf8c1357b63957
 X
 (src\com\marist\mscs721\package-info.java,8\e\8e5eaee33e1f03f6472890c20d4830128a7bd955
-Y
-)src\com\marist\mscs721\RoomScheduler.java,c\2\c2695992a1a8fd5f49edfbba74b16103b7b763f2
 Q
 !src\com\marist\mscs721\Errors.txt,3\7\370ce689ec25ff7c3590d6298517017736f8ad3d
-P
- src\com\marist\mscs721\Room.java,8\b\8b845e945c339aaa7e1cd8329ec539fb83a8151e
 S
 #src\com\marist\mscs721\Meeting.java,b\d\bda7565c1bd1d97a3d0672d452a5cec1f28a057e
+P
+ src\com\marist\mscs721\Room.java,8\b\8b845e945c339aaa7e1cd8329ec539fb83a8151e
+Y
+)src\com\marist\mscs721\RoomScheduler.java,c\2\c2695992a1a8fd5f49edfbba74b16103b7b763f2
--- src/com/marist/mscs721/RoomScheduler.java
+++ src/com/marist/mscs721/RoomScheduler.java
@@ -9,7 +9,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 /**
  * This class is the main portion for the RoomScheduler program. It is a console based interface the prompts a user to
@@ -23,10 +23,11 @@ import java.util.logging.Logger;
 public class RoomScheduler {
   protected static Scanner keyboard = new Scanner(System.in);
   static final String SEPARATOR = "---------------------";
+  static final String ERR_MSG1 = "Integers only, please. Try again.";
+  static final String LOG_FILE = "my.log";
   private static Logger LOGGER = Logger.getLogger("InfoLogging");
 
-
-  private RoomScheduler() {
+  private RoomScheduler() throws IOException {
     throw new IllegalAccessError("Utility class");
   }
 
@@ -35,7 +36,7 @@ public class RoomScheduler {
    * value that corresponds to one of the methods described above. After a number is entered, it then calls that
    * specific function that takes a list of rooms as a parameter. This is also where the list of rooms is created.
    */
-  public static void main(String[] args){
+  public static void main(String[] args) throws IOException {
     Boolean end = false;
     ArrayList<Room> rooms = new ArrayList<Room>();
 
@@ -78,30 +79,35 @@ public class RoomScheduler {
    * This method lists the schedules for a specified room. It calls getRoomName and the user is prompted to enter the
    * room number for an existing room in the room list. The schedule for that room is then displayed.
    *
-   * @param  roomList  the list of rooms that have been created
-   * @return   String   an empty string
+   * @param roomList the list of rooms that have been created
+   * @return String   an empty string
    */
   protected static String listSchedule(ArrayList<Room> roomList) {
     String roomName = getRoomName();
     System.out.println(roomName + " Schedule");
     System.out.println(SEPARATOR);
 
-    for (Meeting m : getRoomFromName(roomList, roomName).getMeetings()) {
-      System.out.println(m.toString());
+
+    if (getRoomFromName(roomList, roomName).getCapacity() == -1) {
+      return "Room does not exist.";
+    } else {
+      for (Meeting m : getRoomFromName(roomList, roomName).getMeetings()) {
+        System.out.println(m.toString());
+      }
+      System.out.println(SEPARATOR);
+
+      return "";
     }
 
-    System.out.println(SEPARATOR);
-
-    return "";
   }
 
   /**
    * This is the main menu for the program. It lists all of the options that the user can choose from, and then accepts
    * the user's input for a number.
    *
-   * @return    int  the user's number that they entered
+   * @return int  the user's number that they entered
    */
-  protected static int mainMenu() throws InputMismatchException{
+  protected static int mainMenu() throws IOException {
     int selection = 0;
 
     System.out.println("Main Menu:");
@@ -116,17 +122,18 @@ public class RoomScheduler {
     System.out.println("Enter your selection: ");
 
     // Forces integer input
-    try
-    {
+    try {
       selection = keyboard.nextInt();
-    }
-    catch (InputMismatchException exception)
-    {
-      System.out.println("Integers only, please.");
+    } catch (InputMismatchException exception) {
+      LOGGER.setUseParentHandlers(false);
+      FileHandler fileHandler = new FileHandler(LOG_FILE);
+      LOGGER.addHandler(fileHandler);
+      LOGGER.log(Level.INFO, ERR_MSG1, exception);
+      System.out.println(ERR_MSG1);
       keyboard.nextLine();
     }
 
-    if (selection < 1 || selection > 8){
+    if (selection < 1 || selection > 8) {
       System.out.println("Please enter a valid number from 1 - 8.");
     }
 
@@ -137,24 +144,26 @@ public class RoomScheduler {
    * This method adds a room to the room list. It prompts the user for a room name and a room capacity. The information
    * is then saved and added to a newly created room object. The room object is then added to the room list.
    *
-   * @param  roomList  the list of rooms that have been created
-   * @return   String   a string that displays the room name and that it was created successfully.
+   * @param roomList the list of rooms that have been created
+   * @return String   a string that displays the room name and that it was created successfully.
    */
-  protected static String addRoom(ArrayList<Room> roomList) {
-    int capacity = 0;
+  protected static String addRoom(ArrayList<Room> roomList) throws IOException {
+    int capacity;
 
     System.out.println("Add a room:");
     String name = getRoomName();
     System.out.println("Room capacity?");
 
     // Forces input to be an integer
-    try
-    {
+    try {
       capacity = keyboard.nextInt();
-    }
-    catch (InputMismatchException exception)
-    {
-      System.out.println("Integers only, please.");
+    } catch (InputMismatchException exception) {
+      LOGGER.setUseParentHandlers(false);
+      FileHandler fileHandler = new FileHandler(LOG_FILE);
+      LOGGER.addHandler(fileHandler);
+      LOGGER.log(Level.INFO, ERR_MSG1, exception);
+      System.out.println(ERR_MSG1);
+      keyboard.nextLine();
       return "";
     }
 
@@ -169,14 +178,14 @@ public class RoomScheduler {
    * room name with the rooms in the room list to see if that room exists. If that room exists, it is then removed from
    * the list.
    *
-   * @param  roomList  the list of rooms that have been created
-   * @return  String    a string that states the room was removed successfully.
+   * @param roomList the list of rooms that have been created
+   * @return String    a string that states the room was removed successfully.
    */
   protected static String removeRoom(ArrayList<Room> roomList) {
     System.out.println("Remove a room:");
 
     // Checks if room exists
-    if(findRoomIndex(roomList, getRoomName()) == -1){
+    if (findRoomIndex(roomList, getRoomName()) == -1) {
       return "";
     }
 
@@ -189,8 +198,8 @@ public class RoomScheduler {
    * This method lists the rooms in the room list. It iterates through each room in the room list and displays the room
    * name and capacity to the user.
    *
-   * @param  roomList  the list of rooms that have been created
-   * @return  String    a string that displays how many rooms are in the room list
+   * @param roomList the list of rooms that have been created
+   * @return String    a string that displays how many rooms are in the room list
    */
   protected static String listRooms(ArrayList<Room> roomList) {
     System.out.println("Room Name - Capacity");
@@ -209,104 +218,94 @@ public class RoomScheduler {
    * This method schedules a meeting for a specified room. It prompts the user for an existing room name. It then asks
    * for a start date and time, and an end date and time. It then saves the dates and times as a Timestamp for the start
    * and end dates/times.
-   *
+   * <p>
    * It then checks to see if there is any schedule conflict with an existing meeting. If there is, then the method
    * returns an empty string.
-   *
+   * <p>
    * If there is no schedule conflict, then it prompts the user for a subject for the meeting. It then creates the
    * meeting, and adds the meeting to the room.
    *
-   * @param  roomList  the list of rooms that have been created
-   * @return  String    an empty string
+   * @param roomList the list of rooms that have been created
+   * @return String    an empty string
    */
   protected static String scheduleRoom(ArrayList<Room> roomList) {
     System.out.println("Schedule a room:");
     String name = getRoomName();
     Room curRoom = getRoomFromName(roomList, name);
 
-    // Makes sure room exists before continuing
-    if(curRoom == null){
+    if (curRoom.getCapacity() == -1) {
       return "";
-    }
+    } else {
+      System.out.println("Start Date? (yyyy-mm-dd):");
+      String startDate = keyboard.nextLine();
+      System.out.println("Start Time? (h:mm):");
+      String startTime = keyboard.nextLine();
+      startTime = startTime + ":00.0";
 
-    System.out.println("Start Date? (yyyy-mm-dd):");
-    String startDate = keyboard.nextLine();
-    System.out.println("Start Time? (h:mm):");
-    String startTime = keyboard.nextLine();
-    startTime = startTime + ":00.0";
+      System.out.println("End Date? (yyyy-mm-dd):");
+      String endDate = keyboard.nextLine();
+      System.out.println("End Time? (h:mm):");
+      String endTime = keyboard.nextLine();
+      endTime = endTime + ":00.0";
 
-    System.out.println("End Date? (yyyy-mm-dd):");
-    String endDate = keyboard.nextLine();
-    System.out.println("End Time? (h:mm):");
-    String endTime = keyboard.nextLine();
-    endTime = endTime + ":00.0";
+      // Checks if user input matches proper Timestamp format
+      SimpleDateFormat format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
+      try {
+        format.parse(startDate + " " + startTime);
+      } catch (ParseException e) {
+        System.out.println("Invalid format for start date or start time.");
+        return "";
+      }
 
-    // Checks if user input matches proper Timestamp format
-    SimpleDateFormat format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
-    try{
-      format.parse(startDate + " " + startTime);
-    }
-    catch(ParseException e)
-    {
-      System.out.println("Invalid format for start date or start time.");
-      return "";
-    }
+      Timestamp startTimestamp = Timestamp.valueOf(startDate + " " + startTime);
 
-    Timestamp startTimestamp = Timestamp.valueOf(startDate + " " + startTime);
+      // Same as above but for endTimestamp
+      try {
+        format.parse(endDate + " " + endTime);
+      } catch (ParseException e) {
+        System.out.println("Invalid format for end date or end time.");
+        return "";
+      }
 
-    // Same as above but for endTimestamp
-    try{
-      format.parse(endDate + " " + endTime);
-    }
-    catch(ParseException e)
-    {
-      System.out.println("Invalid format for end date or end time.");
-      return "";
-    }
-
-    Timestamp endTimestamp = Timestamp.valueOf(endDate + " " + endTime);
+      Timestamp endTimestamp = Timestamp.valueOf(endDate + " " + endTime);
 
     /* Checks if a start time starts in the middle of an existing meeting, checks if an end time ends in the middle of
      * an existing meeting, checks if a meeting starts before and ends after an existing meeting, checks if a start time
      * is the same start time as an existing meeting, and checks if the end time is the same as the end time of
      * an existing meeting.
      */
-    if(!roomList.isEmpty()) {
-      for (int i = 0; i < roomList.size(); i++) {
-        if (startTimestamp.after(getRoomFromName(roomList, name).getMeetings().get(0).getStartTime())
-            && startTimestamp.before(getRoomFromName(roomList, name).getMeetings().get(0).getStopTime())) {
-          System.out.println("Sorry, a meeting cannot start in the middle of another meeting");
-          return "";
-        }
-        else if (startTimestamp.before(getRoomFromName(roomList, name).getMeetings().get(0).getStartTime())
-            && endTimestamp.after(getRoomFromName(roomList, name).getMeetings().get(0).getStopTime())){
-          System.out.println("Sorry, a meeting cannot start before and end after another meeting.");
-          return "";
-        }
-        else if(endTimestamp.after(getRoomFromName(roomList, name).getMeetings().get(0).getStartTime())
-            && endTimestamp.before(getRoomFromName(roomList, name).getMeetings().get(0).getStopTime())){
-          System.out.println("Sorry, a meeting cannot end in the middle of another meeting");
-          return "";
-        }
-        else if(startTimestamp.equals(getRoomFromName(roomList, name).getMeetings().get(0).getStartTime())){
-          System.out.println("Sorry, a meeting start time cannot be the same as another meeting start time");
-          return "";
-        }
-        else if(endTimestamp.equals(getRoomFromName(roomList, name).getMeetings().get(0).getStopTime())){
-          System.out.println("Sorry, a meeting end time cannot be the same as another meeting end time");
-          return "";
+      if (!roomList.isEmpty()) {
+        for (int i = 0; i < roomList.size(); i++) {
+          if (startTimestamp.after(getRoomFromName(roomList, name).getMeetings().get(0).getStartTime())
+              && startTimestamp.before(getRoomFromName(roomList, name).getMeetings().get(0).getStopTime())) {
+            System.out.println("Sorry, a meeting cannot start in the middle of another meeting");
+            return "";
+          } else if (startTimestamp.before(getRoomFromName(roomList, name).getMeetings().get(0).getStartTime())
+              && endTimestamp.after(getRoomFromName(roomList, name).getMeetings().get(0).getStopTime())) {
+            System.out.println("Sorry, a meeting cannot start before and end after another meeting.");
+            return "";
+          } else if (endTimestamp.after(getRoomFromName(roomList, name).getMeetings().get(0).getStartTime())
+              && endTimestamp.before(getRoomFromName(roomList, name).getMeetings().get(0).getStopTime())) {
+            System.out.println("Sorry, a meeting cannot end in the middle of another meeting");
+            return "";
+          } else if (startTimestamp.equals(getRoomFromName(roomList, name).getMeetings().get(0).getStartTime())) {
+            System.out.println("Sorry, a meeting start time cannot be the same as another meeting start time");
+            return "";
+          } else if (endTimestamp.equals(getRoomFromName(roomList, name).getMeetings().get(0).getStopTime())) {
+            System.out.println("Sorry, a meeting end time cannot be the same as another meeting end time");
+            return "";
+          }
         }
       }
+      System.out.println("Subject?");
+      String subject = keyboard.nextLine();
+
+      Meeting meeting = new Meeting(startTimestamp, endTimestamp, subject);
+
+      curRoom.addMeeting(meeting);
+
+      return "Successfully scheduled meeting!\n";
     }
-
-    System.out.println("Subject?");
-    String subject = keyboard.nextLine();
-
-    Meeting meeting = new Meeting(startTimestamp, endTimestamp, subject);
-
-    curRoom.addMeeting(meeting);
-
-    return "Successfully scheduled meeting!\n";
   }
 
   /**
@@ -314,10 +313,10 @@ public class RoomScheduler {
    * time. It uses gson to convert the roomlist into JSON, and then saves it to a file called "file.json" and stores it
    * in the RoomScheduler folder.
    *
-   * @param  roomList  the list of rooms that have been created
-   * @return    String  a string that displays that the objects were successfully exported to JSON
+   * @param roomList the list of rooms that have been created
+   * @return String  a string that displays that the objects were successfully exported to JSON
    */
-  protected static String exportToJson(ArrayList<Room> roomList){
+  protected static String exportToJson(ArrayList<Room> roomList) throws IOException {
     Gson gson = new Gson();
 
     String json = gson.toJson(roomList);
@@ -328,7 +327,10 @@ public class RoomScheduler {
       gson.toJson(roomList, writer);
 
     } catch (IOException e) {
-      LOGGER.info(e.getMessage());
+      LOGGER.setUseParentHandlers(false);
+      FileHandler fileHandler = new FileHandler(LOG_FILE);
+      LOGGER.addHandler(fileHandler);
+      LOGGER.log(Level.INFO, "IOException", e);
     }
 
     return "Successfully exported objects to JSON!";
@@ -338,22 +340,25 @@ public class RoomScheduler {
    * This method allows a user to import a JSON file into the program. It looks for a file name "file.json" in the
    * RoomScheduler folder and converts it into an array. It then adds that array to the room list in the program.
    *
-   * @param  roomList  the list of rooms that have been created
-   * @return    String  a string that displays that the import was successful
+   * @param roomList the list of rooms that have been created
+   * @return String  a string that displays that the import was successful
    */
-  protected static String importFromJson(ArrayList<Room> roomList){
+  protected static String importFromJson(ArrayList<Room> roomList) throws IOException {
     Gson gson = new Gson();
 
     try (Reader reader = new FileReader("file.json")) {
 
-        Room[] roomArr = gson.fromJson(reader, Room[].class);
+      Room[] roomArr = gson.fromJson(reader, Room[].class);
 
-        for (int i = 0; i < roomArr.length; i++) {
-          roomList.add(roomArr[i]);
-        }
+      for (int i = 0; i < roomArr.length; i++) {
+        roomList.add(roomArr[i]);
+      }
 
     } catch (IOException e) {
-      LOGGER.info(e.toString());
+      LOGGER.setUseParentHandlers(false);
+      FileHandler fileHandler = new FileHandler(LOG_FILE);
+      LOGGER.addHandler(fileHandler);
+      LOGGER.log(Level.INFO, "IOException", e);
     }
 
     return "Successfully imported JSON to objects!";
@@ -363,16 +368,15 @@ public class RoomScheduler {
    * This method gets the room object based on the user inputted room name. It takes the room list and name and checks
    * if the room is in the room list. If it is, then it returns the room.
    *
-   * @param  roomList  the list of rooms that have been created
-   * @param  name      the name of the room that the user entered
-   * @return  Room    the room that was found in the room list
+   * @param roomList the list of rooms that have been created
+   * @param name     the name of the room that the user entered
+   * @return Room    the room that was found in the room list
    */
   protected static Room getRoomFromName(ArrayList<Room> roomList, String name) {
     // Checks if room exists in roomList
-    if(findRoomIndex(roomList,name) == -1){
-      return null;
+    if (findRoomIndex(roomList, name) == -1) {
+      return new Room("error", -1);
     }
-
     return roomList.get(findRoomIndex(roomList, name));
   }
 
@@ -381,14 +385,14 @@ public class RoomScheduler {
    * index starts at 0 and compares each room in the list to the user submitted room name. If the room is found, then
    * it exits the for loop. If it is not found then it increments the room index.
    *
-   * @param  roomList  the list of rooms that have been created
-   * @param  roomName  the name of the room that the user entered
-   * @return    int     the roomIndex where the room is located
+   * @param roomList the list of rooms that have been created
+   * @param roomName the name of the room that the user entered
+   * @return int     the roomIndex where the room is located
    */
   protected static int findRoomIndex(ArrayList<Room> roomList, String roomName) {
     int roomIndex = 0;
 
-    if(roomList.isEmpty()){
+    if (roomList.isEmpty()) {
       System.out.println("There are no rooms with that name.");
       return -1;
     }
@@ -401,7 +405,7 @@ public class RoomScheduler {
     }
 
     // If no room name matches the rooms in the room list, it returns a -1
-    if(roomIndex >= roomList.size()){
+    if (roomIndex >= roomList.size()) {
       System.out.println("There are no rooms with that name.");
       return -1;
     }
@@ -412,7 +416,7 @@ public class RoomScheduler {
   /**
    * This method prompts the user for a room name and returns the user's input.
    *
-   * @return  String  the name of the room that the user entered.
+   * @return String  the name of the room that the user entered.
    */
   protected static String getRoomName() {
     String selection;
